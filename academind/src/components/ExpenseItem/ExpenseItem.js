73e182b4769d1f3c1e4dd11ee36d5c19fc969a2a
dlaepeul <dlaepeul@gmail.com>
2022-07-11 23:56:13 +0900

    feat:add java script logic to component

--- academind/src/components/ExpenseItem/ExpenseItem.js
+++ academind/src/components/ExpenseItem/ExpenseItem.js
@@ -1,8 +1,16 @@
 import './index.css'
 
 function ExpenseItem({ expenseItem: { date, title, amount }}) {
+  const year = date.getFullYear();
+  const month = date.toLocaleDateString('en-US', { month: "long"});
+  const day = date.toLocaleDateString('en-US', { day: "numeric"});
+
   return <div className="expense-item">
-    <div>{date.toISOString()}</div>
+    <div className="expense-item__date">
+      <span>{year}</span>
+      <span>{month}</span>
+      <span>{day}</span>
+    </div>
     <div className="expense-item__description">
       <h2>{title}</h2>
       <div className="expense-item__price"> ${amount} </div>
