677df1af9b3592406bbfca71abec1af339e64320
dlaepeul <dlaepeul@gmail.com>
2022-07-11 22:55:51 +0900

    feat:apply props to component

--- academind/src/App.js
+++ academind/src/App.js
@@ -1,9 +1,39 @@
 import ExpenseItem from "./components/ExpenseItem/ExpenseItem";
 
 function App() {
+  const expenses = [
+    {
+      id: 1,
+      title: "Toilet Paper",
+      amount: 94.12,
+      date: new Date(2021, 7, 14),
+    },
+    {
+      id: 2,
+      title: "New Tv",
+      amount: 799.49,
+      date: new Date(2022, 2, 12),
+    },
+    {
+      id: 3,
+      title: "Car Insurance",
+      amount: 294.67,
+      date: new Date(2022, 2, 28),
+    },
+    {
+      id: 4,
+      title: "New Desk (Wooden)",
+      amount: 450,
+      date: new Date(2022, 5, 12),
+    }
+  ]
+
   return (
     <div>
-      <ExpenseItem/>
+      <ExpenseItem expenseItem={expenses[0]}/>
+      <ExpenseItem expenseItem={expenses[1]}/>
+      <ExpenseItem expenseItem={expenses[2]}/>
+      <ExpenseItem expenseItem={expenses[3]}/>
     </div>
   );
 }
--- academind/src/components/ExpenseItem/ExpenseItem.js
+++ academind/src/components/ExpenseItem/ExpenseItem.js
@@ -1,11 +1,11 @@
 import './index.css'
 
-function ExpenseItem() {
+function ExpenseItem({ expenseItem: { date, title, amount }}) {
   return <div className="expense-item">
-    <div>March 28th 2021</div>
+    <div>{date.toISOString()}</div>
     <div className="expense-item__description">
-      <h2>Car Insurance</h2>
-      <div className="expense-item__price"> $294.67 </div>
+      <h2>{title}</h2>
+      <div className="expense-item__price"> ${amount} </div>
     </div>
   </div>
 }
