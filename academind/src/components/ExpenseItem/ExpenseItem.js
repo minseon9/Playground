0c7f63ca7e493be3abf7b13224c942de60d990e8
dlaepeul <dlaepeul@gmail.com>
2022-07-28 23:21:16 +0900

    feat:apply useState to update component

--- academind/src/components/ExpenseItem/ExpenseItem.js
+++ academind/src/components/ExpenseItem/ExpenseItem.js
@@ -1,14 +1,18 @@
+import React, { useState } from 'react'
 import ExpenseDate from "./ExpenseDate";
 import Card from '../UI';
 import './ExpenseItem.css'
 
 function ExpenseItem({ expenseItem: { date, title, amount }}) {
+  const [expenseTitle, setExpenseTitle] = useState(title);
+
   return <Card className="expense-item">
     <ExpenseDate date={date}/>
     <div className="expense-item__description">
-      <h2>{title}</h2>
+      <h2>{expenseTitle}</h2>
       <div className="expense-item__price"> ${amount} </div>
     </div>
+    <button onClick={()=> setExpenseTitle('Update!')}>Change Title</button>
   </Card>
 }
 
