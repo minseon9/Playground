22952abaddd724aa41d9c2acfc6e3c514aa985b5
dlaepeul <dlaepeul@gmail.com>
2023-03-18 21:58:43 +0900

    delete unusable stuffs

--- leetcode/6.ZigzagConversion/arguments.ts
+++
@@ -1,4 +0,0 @@
-export const Arguments = {
-  s: 'Apalindromeisaword,phrase,number,orothersequenceofunitsthatcanbereadthesamewayineitherdirection,withgeneralallowancesforadjustmentstopunctuationandworddividers.',
-  numRows: 3
-}
