7d4c71a6a5f4a88fd60dc5bcd1ab57e4fcd0d59b
dlaepeul <dlaepeul@gmail.com>
2023-03-28 23:34:17 +0900

    feat:expense filter & expense filter handler with lifting up

--- academind/src/components/ExpenseItem/Expenses.js
+++ academind/src/components/ExpenseItem/Expenses.js
@@ -1,10 +1,17 @@
-import ExpenseItem from "./ExpenseItem";
+import {useState} from "react";
 import Card from "../UI";
+import ExpenseItem from "./ExpenseItem";
+import ExpensesFilter from "../ExpenseFilter/ExpenseFilter";
 import './index.css'
 
 function Expenses({ expenseList }) {
+  const [filteredYear, setFilteredYear] = useState(2022);
+  const filterExpenseItemHandler = selectedYear => {
+    setFilteredYear(selectedYear);
+  }
+
   const expenseItems = expenseList.map((expenseItem) => <ExpenseItem expenseItem={expenseItem} />)
-  return <Card className="expenses">{expenseItems}</Card>
+  return <Card className="expenses"> <ExpensesFilter selected={filteredYear} onFilterExpenseItem={filterExpenseItemHandler}/> {expenseItems}</Card>
 }
 
 export default Expenses;
